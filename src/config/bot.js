0f4ef0b31915224c2a15ae80390a69376edf0e19
doihavetodothis-idc <declanvandeventer339@gmail.com>
2026-06-26 16:52:03 +1200

    Updated bot changes
    Commit verified

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made for Nico's servers.",
+        name: "Assisting Frosty with his servers.",
         // Activity type number (0 = Playing).
         type: 0, 
       },
